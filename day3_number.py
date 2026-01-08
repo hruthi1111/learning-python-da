bd11e679c403d4f9151d4d437edae63102080e5d
Hruthi <hruthibharathib@gmail.com>
2026-01-08 21:47:20 +0530

    Create day3_number.py

---
+++ day3_number.py
@@ -0,0 +1,10 @@
+# working with numbers
+marks=[]
+total_subs=int(input('enter no of subjects:'))
+for i in range(total_subs):
+  markks=int(input(f'enter marks{i+1}:'))
+  marks.append(marks)
+total=sum(marks)
+avg=total/total_subs
+print('Total Marks:',total)
+print('Average marks:',avg)
