c95172df5887d83c3c6d35086a082e7507b41f7f
Hruthi <hruthibharathib@gmail.com>
2026-01-09 21:57:55 +0530

    Create day4_number.py
    Grade system

---
+++ day4_number.py
@@ -0,0 +1,12 @@
+# numbers continution with condition statments
+marks=int(input('enter your marks'))
+if marks>90:
+  print('Grade S')
+elif marks>=80:
+  print('Grade A')
+elif marks>=70:
+  print('Grade B')
+elif marks>=60:
+  print('Grade c')
+else:
+  print('Fail')
