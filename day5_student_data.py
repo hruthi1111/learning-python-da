a5d8c3978517b398df0ab6574ae627f932f1e4d6
Hruthi <hruthibharathib@gmail.com>
2026-01-10 22:46:54 +0530

    Create day5_student_data.py
    Restore day4 student data file
    which iss actually day5 but i mistakenly typed 4

---
+++ day5_student_data.py
@@ -0,0 +1,9 @@
+# Day 5- Student data using dictionary
+student={
+  'name':input('enter name:'),
+  'halltickno':int(input('enter your Roll No:'),
+  'marks':int(input('enter marks:'))
+}
+print('Student Name:',student[name])
+print('Student Roll No:',student[halltickno])
+print('Student Marks:',student[marks])
